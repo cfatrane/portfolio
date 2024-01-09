1e737471fa922a434ff268ef81d489f6e6035abc
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-01-09 15:59:41 +1100

    start from template and minor update

---
+++ src/constants/social/index.ts
@@ -0,0 +1,6 @@
+const SOCIAL = {
+  twitter: "https://twitter.com/aristoteartem/",
+  instagram: "https://www.instagram.com/aristoteartem/",
+  github: "https://github.com/cfatrane/",
+  linkedin: "https://www.linkedin.com/in/cfatrane/",
+};
