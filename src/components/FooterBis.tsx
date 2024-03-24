0b7e5255e38db2edb3be69f3329441614d30d239
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-03-25 00:01:46 +0100

    uodate props

--- src/components/FooterBis.tsx
+++ src/components/FooterBis.tsx
@@ -1,10 +1,12 @@
+import { JSX, SVGProps } from "react";
+
 import SOCIAL from "@/constants/social";
 
 const navigation = [
   {
     name: "LinkedIn",
     href: SOCIAL.linkedIn,
-    icon: (props) => (
+    icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
           clipRule="evenodd"
@@ -17,7 +19,7 @@ const navigation = [
   {
     name: "GitHub",
     href: SOCIAL.github,
-    icon: (props) => (
+    icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
           clipRule="evenodd"
@@ -30,7 +32,7 @@ const navigation = [
   {
     name: "Malt",
     href: SOCIAL.malt,
-    icon: (props) => (
+    icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
       <svg fill="currentColor" viewBox="0 0 512 512" {...props}>
         <path
           clipRule="evenodd"
@@ -43,7 +45,7 @@ const navigation = [
   {
     name: "Instagram",
     href: SOCIAL.instagram,
-    icon: (props) => (
+    icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
           clipRule="evenodd"
@@ -56,7 +58,7 @@ const navigation = [
   {
     name: "X",
     href: SOCIAL.twitter,
-    icon: (props) => (
+    icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
       </svg>
