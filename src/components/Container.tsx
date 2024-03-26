61379d2b4f53ff12a24fc11181dfd7c279b5b62b
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-03-26 14:22:49 +0100

    add container from tailwind ui

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -38,3 +38,9 @@ export const Container = forwardRef<
     </ContainerOuter>
   );
 });
+
+// export function Container({ children }) {
+//   return (
+//     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">{children}</div>
+//   );
+// }
