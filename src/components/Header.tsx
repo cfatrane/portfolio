b74b773c9f8a5030febe15dc1a0c086db48ba31a
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-03-26 21:32:27 +0100

    change butto

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,6 +13,7 @@ import {
   NavbarMenu,
   NavbarMenuItem,
   Link,
+  Button,
 } from "@nextui-org/react";
 
 const navigation = [
@@ -31,10 +32,12 @@ function NavItem({
   const isActive = usePathname() === href;
 
   return (
-    <NavbarItem className="rounded p-2 hover:bg-blue-600" isActive={isActive}>
-      <Link color={isActive ? "primary" : "foreground"} href={href}>
-        {children}
-      </Link>
+    <NavbarItem isActive={isActive}>
+      <Button variant="light">
+        <Link color={isActive ? "primary" : "foreground"} href={href}>
+          {children}
+        </Link>
+      </Button>
     </NavbarItem>
   );
 }
@@ -75,24 +78,26 @@ export function Header() {
         />
 
         <NavbarBrand className="flex items-center">
-          <Link color={isHomePage ? "primary" : "foreground"} href="/">
-            <svg
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="1.5"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+          <Button variant="light">
+            <Link color={isHomePage ? "primary" : "foreground"} href="/">
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
 
-            <p className="ml-2 font-bold text-inherit text-white">Home</p>
-          </Link>
+              <p className="ml-2 font-bold text-inherit text-white">Home</p>
+            </Link>
+          </Button>
         </NavbarBrand>
       </NavbarContent>
 
