4473ea4265687413833f9639373941bb889b0f65
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-03-29 21:30:47 +0100

    remove graysale from photo

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,16 +5,18 @@ import { HoverEffect } from "@/components/ui/card-hover-effect";
 
 import { PROJECTS } from "@/constants/projects";
 
+const title = "Les projets auxquels j'ai pu contribuer.";
+
 export const metadata: Metadata = {
   title: "Projects",
-  description: "Les projets auxquels j'ai pu contribuer.",
+  description: title,
 };
 
 export default function Projects() {
   return (
     <SimpleLayout
-      intro="J'ai travaillé sur des tonnes de petits projets au fil des ans, mais voici ceux dont je suis le plus fier. Beaucoup d'entre eux sont open-source, donc si vous voyez quelque chose qui vous intéresse, regardez le code et contribuez si vous avez des idées pour l'améliorer."
-      title="Les projets auxquels j'ai pu contribuer."
+      intro="J'ai travaillé sur des tonnes de petits projets au fil des ans, en voici quelques-un. Beaucoup d'entre eux sont open-source, donc si vous voyez quelque chose qui vous intéresse, regardez le code et contribuez si vous avez des idées pour l'améliorer."
+      title={title}
     >
       <div className="mx-auto max-w-5xl px-8">
         <HoverEffect items={PROJECTS} withMeteors />
