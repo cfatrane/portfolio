a23be698538b4fbbf09a861f7bdb057b642b6521
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-04-25 19:36:07 +0200

    remove grid

--- src/components/SimpleLayout.tsx
+++ src/components/SimpleLayout.tsx
@@ -21,13 +21,7 @@ export function SimpleLayout({
         </p>
       </header>
 
-      <div className="relative w-full bg-white bg-grid-black/[0.2] dark:bg-black dark:bg-grid-white/[0.2]">
-        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-
-        <div>
-          {children && <div className=" mt-16 sm:mt-20">{children}</div>}
-        </div>
-      </div>
+      {children && <div className=" mt-16 sm:mt-20">{children}</div>}
     </Container>
   );
 }
