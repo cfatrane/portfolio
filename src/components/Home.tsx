36a052886d5b6a4351ec487e20e97d1a706f432a
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-04-25 19:43:21 +0200

    add blend

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -22,7 +22,7 @@ function Home() {
 
   return (
     // <WavyBackground blur={10} className="mx-auto max-w-4xl" waveWidth={50}>
-    <WavyBackground className="mx-auto max-w-4xl">
+    <WavyBackground className="mx-auto max-w-4xl mix-blend-difference">
       <div className="flex flex-col items-center justify-center">
         <p className="inter-var text-center text-2xl font-bold text-white md:text-4xl lg:text-7xl">
           Charles-Édouard Fatrane
