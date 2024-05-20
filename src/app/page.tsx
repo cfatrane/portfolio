8332a5140603f8fe0bc5c352536089d7d64db474
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-05-20 16:19:00 +0200

    change color of main app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,11 +18,11 @@ export default async function Index() {
 
       <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div className="text-center">
-          <h1 className="text-4xl tracking-tight text-white sm:text-6xl">
+          <h1 className="text-4xl tracking-tight text-primary sm:text-6xl">
             Charles-Édouard Fatrane
           </h1>
 
-          <p className="mt-6 text-lg leading-8 text-gray-300">
+          <p className="mt-6 text-lg leading-8 text-primary/75">
             Développeur Web & Mobile
           </p>
         </div>
