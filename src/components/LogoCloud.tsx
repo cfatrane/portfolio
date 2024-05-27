bddd4e4e8218c3a40ecbc2cdf6269ac172da1e28
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-05-27 18:16:13 +0200

    update size and layout

--- src/components/LogoCloud.tsx
+++ src/components/LogoCloud.tsx
@@ -34,42 +34,44 @@ function LogoCloudItem({
 
 export function LogoCloud() {
   return (
-    <div className="flex flex-col items-center justify-center bg-secondary py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <h2 className="text-center text-lg font-semibold leading-8 text-primary">
-          J'ai collaborer avec de grands groupes et startups françaises
-        </h2>
+    <div className="flex flex-col items-center">
+      <div className="w-full shrink-0">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <h2 className="text-center text-lg font-semibold leading-8 text-primary">
+            J'ai collaborer avec de grands groupes et startups françaises
+          </h2>
 
-        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-          <LogoCloudItem
-            alt="bnp"
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            icon={BNPLogo}
-          />
+          <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+            <LogoCloudItem
+              alt="bnp"
+              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+              icon={BNPLogo}
+            />
 
-          <LogoCloudItem
-            alt="le-figaro"
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            icon={LeFigaroLogo}
-          />
+            <LogoCloudItem
+              alt="le-figaro"
+              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+              icon={LeFigaroLogo}
+            />
 
-          <LogoCloudItem
-            alt="payplug"
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            icon={PayplugLogo}
-          />
+            <LogoCloudItem
+              alt="payplug"
+              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+              icon={PayplugLogo}
+            />
 
-          <LogoCloudItem
-            alt="la-poste"
-            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-            icon={LaPosteLogo}
-          />
+            <LogoCloudItem
+              alt="la-poste"
+              className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+              icon={LaPosteLogo}
+            />
 
-          <LogoCloudItem
-            alt="kinetix"
-            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-            icon={KinetixLogo}
-          />
+            <LogoCloudItem
+              alt="kinetix"
+              className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+              icon={KinetixLogo}
+            />
+          </div>
         </div>
       </div>
 
